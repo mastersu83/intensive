9bc818440e8c492ed3dd4b7bd581c0af5952ac16
master <mastersu93@gmail.com>
2022-12-17 03:26:27 +0300

    bubbleSort

---
+++ bubble_sort.js
@@ -0,0 +1,17 @@
+const arr = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+const bubbleSort = (arr) => {
+  const tmp = [...arr];
+  for (let i = 0; i < tmp.length - 1; i++) {
+    for (let j = 0; j < tmp.length - 1; j++) {
+      if (tmp[j] > tmp[j + 1]) {
+        let swap = tmp[j];
+        tmp[j] = tmp[j + 1];
+        tmp[j + 1] = swap;
+      }
+    }
+  }
+  console.log(tmp);
+};
+
+bubbleSort(arr);
